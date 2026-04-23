ee78e852de997202ede79ba8331451a0d0fdf7e2
FaberSantiago <faberforero@gmail.com>
2026-04-23 14:53:55 -0500

    Add files via upload

---
+++ assets/404NotFound-pzvnVzfI.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c}from"./index-C0ohmKH_.js";const n={};function t(r,_){return o(),c("h1",null,"Page Not Found")}const s=e(n,[["render",t]]);export{s as default};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/content/sw.js', { scope: '/content/' })})}
